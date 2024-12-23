1252dadd1d09e31bd361e3f7b4280864fbc18aa2
RObbinChistopher <robbinc042@gmail.com>
2024-12-23 14:43:16 -0800

    config productions

--- resources/js/config.js
+++ resources/js/config.js
@@ -3,12 +3,12 @@ const config = {
     development: {
         apiBaseUrl: 'http://localhost:8000/api/'
     },
-    production: {
+    productions: {
         apiBaseUrl: 'http://51.20.181.199/api/'
     }
 };
 
 // Manually select the environment
-const environment = 'production'; // Change to 'production' for production
+const environment = 'productions'; // Change to 'production' for production
 
 export default config[environment];
