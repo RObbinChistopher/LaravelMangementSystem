0ab41c6f1f8a99d7cf8183efaefa046522568a09
christopher <robbinc042@gmail.com>
2024-12-27 08:49:26 -0800

    add fixes

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,8 +1,5 @@
 // src/config.js
 const config = {
-    development: {
-        apiBaseUrl: 'http://localhost:8000/api/'
-    },
     productions: {
         apiBaseUrl: 'http://51.20.181.199/api/'
     }
