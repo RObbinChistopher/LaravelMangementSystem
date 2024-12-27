9390413e7641d03977744d9baf6b9b0552eb162c
RObbinChistopher <robbinc042@gmail.com>
2024-12-27 16:34:03 +0000

    add to resolve

--- resources/js/config.js
+++ resources/js/config.js
@@ -3,21 +3,13 @@ const config = {
     development: {
         apiBaseUrl: 'http://localhost:8000/api/'
     },
-<<<<<<< HEAD
-    production: {
-        apiBaseUrl: 'https://admin.420finder.net/api/'
-=======
     productions: {
         apiBaseUrl: 'http://51.20.181.199/api/'
->>>>>>> 1252dadd1d09e31bd361e3f7b4280864fbc18aa2
     }
 };
 
 // Manually select the environment
-<<<<<<< HEAD
-const environment = 'development'; // Change to 'production' for production
-=======
-const environment = 'productions'; // Change to 'production' for production
->>>>>>> 1252dadd1d09e31bd361e3f7b4280864fbc18aa2
+const environment = 'productions'; // Change to 'development' for development
 
 export default config[environment];
+
