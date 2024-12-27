5914573c799847a561262b198745b4a38594ef24
christopher <robbinc042@gmail.com>
2024-12-27 11:29:32 -0800

    corsupdate

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')  // Allow all origins or specify your frontend URL
+            ->header('Access-Control-Allow-Origin', 'http://51.20.181.199')  // Allow all origins or specify your frontend URL
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With')
             ->header('Access-Control-Allow-Credentials', 'true');
